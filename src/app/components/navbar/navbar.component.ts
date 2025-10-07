195f85e4779a10330cf4970ef201cf38cde8d698
negi-tushar <negitushar44@gmail.com>
2025-10-07 18:07:22 +0530

    updated whatsapp logic6

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,15 +2,28 @@ import { Component, inject } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 
+declare var bootstrap: any; 
+
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [RouterModule],
   templateUrl: './navbar.component.html',
 })
+
+
 export class NavbarComponent {
   private authService = inject(AuthService);
   private router = inject(Router);
+
+   closeNavbar() {
+    const navbar = document.getElementById('mainNavbar');
+    if (navbar) {
+      const bsCollapse = bootstrap.Collapse.getInstance(navbar) 
+                        || new bootstrap.Collapse(navbar, { toggle: false });
+      bsCollapse.hide();
+    }
+  }
 
   logout(): void {
     this.authService.logout().subscribe(() => {
